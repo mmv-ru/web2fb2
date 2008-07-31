b28f3da6dc51fa9996985dd3aaf893acaaf53ec6
shiza <shiza@6f889d7c-ddb9-4dbb-b7a3-8f9b5b8ae9b2>
2008-07-31 15:12:10 +0000

    add: join many htmls to one fb2

--- web2fb2/widget.py
+++ web2fb2/widget.py
@@ -5,6 +5,8 @@ import cgi, cgitb
 cgitb.enable()
 import traceback
 
+import urlparse
+
 import fb_utils
 import process
 
@@ -32,46 +34,21 @@ def main():
 		base()
 
 def base():
-	
-	form = cgi.FieldStorage()
-	url =  form.getvalue('url', '')
-	
-	log.debug('url %s' %url)
-	if url:
-		is_img =  form.getvalue('img', False)
-		
-		do_it = form.getvalue('doit', False)
-		
-		webutils.print_page(
-			render.widget_base(
-				render.widget_form(url, is_img, do_it),
-				render.widget_descr(
-					'',
-					'',
-					'',
-					'',
-					fb_utils.genres().get_genres_descrs(),
-					fb_utils.genres().get_default(),
-					''
-				)
+
+	webutils.print_page(
+		render.widget_base(
+			render.widget_form('http://', True, False),
+			render.widget_descr(
+				'',
+				'',
+				'',
+				'',
+				fb_utils.genres().get_genres_descrs(),
+				fb_utils.genres().get_default(),
+				''
 			)
 		)
-	
-	else:
-		webutils.print_page(
-			render.widget_base(
-				render.widget_form('http://', True, False),
-				render.widget_descr(
-					'',
-					'',
-					'',
-					'',
-					fb_utils.genres().get_genres_descrs(),
-					fb_utils.genres().get_default(),
-					''
-				)
-			)
-		)
+	)
 	
 def ajax():
 
@@ -90,92 +67,104 @@ def ajax():
 	
 	form = cgi.FieldStorage()
 	
-	params.url =  form.getvalue('url', '')
-	
-	if form.getvalue('img') == 'true':
-		params.is_img = True
-		
-	if form.getvalue('yah2fb') == 'true':
-		params.yah2fb = True
-	
-	descr = fb_utils.description()
-	descr.url = params.url
-	
-	if form.getvalue('autodetect') == 'true':
-		descr.author_first = descr.SELFDETECT
-		descr.author_middle = descr.SELFDETECT
-		descr.author_last = descr.SELFDETECT
-		descr.title = descr.SELFDETECT
-		descr.genre = descr.SELFDETECT
-		descr.lang = descr.SELFDETECT
-	else:
-		descr.title = form.getvalue('title', '').decode('UTF-8')
-		descr.author_first = form.getvalue('author_first', '').decode('UTF-8')
-		descr.author_middle = form.getvalue('author_middle', '').decode('UTF-8')
-		descr.author_last = form.getvalue('author_last', '').decode('UTF-8')
-		descr.genre = form.getvalue('genre', '').decode('UTF-8')
-		descr.lang = form.getvalue('lang', '').decode('UTF-8')
-	
-	params.descr = descr
-	#log.debug(str(params.descr.genre))
-		
-	try:
-		progres = process.do(params, sid, True)
-		
-	except process.SessRet, er:	
-		webutils.print_page( json.write({'tryagain': render.widget_try( er.value['place'], prior.priors.get( er.value['prior'], 'unknown') )}) )
-		log.debug('Try later')
-		
-	except Exception, er:
-			webutils.print_page( json.write({'error': render.widget_error(str(er))}) )
-			log.error('\n------------------------------------------------\n' + traceback.format_exc() + '------------------------------------------------\n')
-
-	else:
-		log.debug(str(progres))
-		
-		if progres.error:
-			webutils.print_page( json.write({'error': render.widget_error(str(progres.error))}) )
-			log.debug('progres return error: %s' % progres.error)
-
-		elif progres.done:
+	urls = []
+	for url in form.getlist('url'):
+		if urlparse.urlparse(url)[1]:
+			urls.append(url)
 			
-			stat = progres.done
+	if not urls:
+		webutils.print_page( json.write({'error': render.widget_error('Bad url')}) )
+	else:
+		log.info('We get urls: %s' % urls)
+	
+		params.urls = urls
+		
+		if form.getvalue('img') == 'true':
+			params.is_img = True
 			
-			webutils.print_page(
-				json.write(
-					{
-						'result':render.widget_result(
-							stat.url,
-							'%.1f' % stat.work_time,
-							stat.img,
-							stat.path + '/' + stat.file_name,
-							stat.file_name,
-							stat.file_size//1024,
-							stat.valid['is_valid'],
-							stat.valid['msg']
-						),
-						'descr':{
-							'title':stat.descr.title.encode('UTF-8'),
-							'author_first':stat.descr.author_first.encode('UTF-8'),
-							'author_middle':stat.descr.author_middle.encode('UTF-8'),
-							'author_last':stat.descr.author_last.encode('UTF-8'),
-							'genre':stat.descr.genre.encode('UTF8'),
-							'lang':stat.descr.lang.encode('UTF8')
-						}
-					}
-				)
-			)
+		if form.getvalue('yah2fb') == 'true':
+			params.yah2fb = True
+		
+		descr = fb_utils.description()
+		descr.urls = params.urls
+		
+		if form.getvalue('autodetect') == 'true':
+			descr.selfdetect = True
 		else:
+			descr.selfdetect = False
 			
-			webutils.print_page(
-				json.write({'progres': render.widget_progres(
-					progres.msgs,
-					progres.level,
-					[x for x in xrange(len(progres.msgs))],
-					progres.level + 1,
-					len(progres.msgs)
-				)})
+			descr.authors.append(
+				{
+					'first': form.getvalue('author_first', '').decode('UTF-8'),
+					'middle': form.getvalue('author_middle', '').decode('UTF-8'),
+					'last': form.getvalue('author_last', '').decode('UTF-8')
+				}
 			)
+			
+			descr.title = form.getvalue('title', '').decode('UTF-8')
+			descr.genre = form.getvalue('genre', '').decode('UTF-8')
+			descr.lang = form.getvalue('lang', '').decode('UTF-8')
+		
+		params.descr = descr
+		#log.debug(str(params.descr.genre))
+			
+		try:
+			progres = process.do(params, sid, True)
+			
+		except process.SessRet, er:	
+			webutils.print_page( json.write({'tryagain': render.widget_try( er.value['place'], prior.priors.get( er.value['prior'], 'unknown') )}) )
+			log.debug('Try later')
+			
+		except Exception, er:
+				webutils.print_page( json.write({'error': render.widget_error(str(er))}) )
+				log.error('\n------------------------------------------------\n' + traceback.format_exc() + '------------------------------------------------\n')
+
+		else:
+			log.debug(str(progres))
+			
+			if progres.error:
+				webutils.print_page( json.write({'error': render.widget_error(str(progres.error))}) )
+				log.debug('progres return error: %s' % progres.error)
+
+			elif progres.done:
+				
+				stat = progres.done
+				
+				webutils.print_page(
+					json.write(
+						{
+							'result':render.widget_result(
+								stat.urls,
+								'%.1f' % stat.work_time,
+								stat.img,
+								stat.path + '/' + stat.file_name,
+								stat.file_name,
+								stat.file_size//1024,
+								stat.valid['is_valid'],
+								stat.valid['msg']
+							),
+							'descr':{
+								'title':stat.descr.title.encode('UTF-8'),
+								'author_first':stat.descr.authors[0]['first'].encode('UTF-8'),
+								'author_middle':stat.descr.authors[0]['middle'].encode('UTF-8'),
+								'author_last':stat.descr.authors[0]['last'].encode('UTF-8'),
+								'genre':stat.descr.genre.encode('UTF8'),
+								'lang':stat.descr.lang.encode('UTF8')
+							}
+						}
+					)
+				)
+			else:
+				
+				webutils.print_page(
+					json.write({'progres': render.widget_progres(
+						progres.msgs,
+						progres.level,
+						[x for x in xrange(len(progres.msgs))],
+						progres.level + 1,
+						len(progres.msgs)
+					)})
+				)
 
 if __name__=='__main__':
 	main()
